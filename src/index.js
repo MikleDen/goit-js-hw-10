fa19ef33b31aabbf3b26e386f745156e0dcf1540
Mykhailo Den <95014048+MikleDen@users.noreply.github.com>
2023-01-04 16:28:25 +0200

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { fetchCountries } from './js/fetchCountries';
 const DEBOUNCE_DELAY = 300;
 
-
 const inputEl = document.getElementById('search-box');
 const listEl = document.querySelector('.country-list');
 const infoEl = document.querySelector('.country-info');
@@ -24,7 +23,9 @@ const inputHandler = e => {
     .then(data => {
       console.log(data);
       if (data.length > 10) {
-        Notify.info('Too many matches found. Please enter a more specific name');
+        Notify.info(
+          'Too many matches found. Please enter a more specific name'
+        );
         return;
       }
       renderMarkup(data);
@@ -52,7 +53,7 @@ const createListMarkup = data => {
   return data
     .map(
       ({ name, flags }) =>
-        `<li><img src="${flags.png}" alt="${name.official}" width="60" height="40">${name.official}</li>`,
+        `<li><img src="${flags.png}" alt="${name.official}" width="60" height="40">${name.official}</li>`
     )
     .join('');
 };
@@ -60,12 +61,12 @@ const createListMarkup = data => {
 const createInfoMarkup = data => {
   return data.map(
     ({ name, capital, population, flags, languages }) =>
-      `<h1><img src="${flags.png}" alt="${name.official}" width="40" height="40">${
+      `<h1><img src="${flags.png}" alt="${
         name.official
-      }</h1>
+      }" width="40" height="40">${name.official}</h1>
       <p>Capital: ${capital}</p>
       <p>Population: ${population}</p>
-      <p>Languages: ${Object.values(languages)}</p>`,
+      <p>Languages: ${Object.values(languages)}</p>`
   );
 };
 
